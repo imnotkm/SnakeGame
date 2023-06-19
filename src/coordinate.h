dc8d26a46ccb487865ec79a282e7a0d19a2c481f
imnotkm <137032832+imnotkm@users.noreply.github.com>
2023-06-20 00:29:05 +0900

    Add files via upload

---
+++ src/coordinate.h
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <ncurses.h>
+#include <cstdlib>
+#include <ctime>
+#include <unistd.h>
+#include <string>
+
+using namespace std;
+
+class coordinate{
+private:
+	int x,y;
+public:
+	coordinate();
+	coordinate(int x, int y);
+
+	void setX(int x);
+	void setY(int y);
+	int getX() const;
+	int getY() const;
+
+    coordinate& getRandomCoordinate();
+
+	coordinate& operator=(const coordinate& v);
+	coordinate& operator+=(const coordinate& v);
+	coordinate operator+(const coordinate& v);
+	coordinate& operator-=(const coordinate& v);
+	coordinate operator-(const coordinate& v);
+	bool operator==(const coordinate& v);
+	bool operator!=(const coordinate& v);
+};
